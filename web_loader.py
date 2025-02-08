1dc5ab65b932022b691291e526f318238e1b4095
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-02-08 20:31:14 +0100

    Update web_loader.py
    code for loading web data

--- web_loader.py
+++ web_loader.py
@@ -1,1 +1,11 @@
+# web_loader.py - Scrape Website Data
+from langchain_community.document_loaders import WebBaseLoader
+import bs4
+
+def load_web_content(url):
+    loader = WebBaseLoader(
+        web_paths=(url,),
+        bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("post-content", "post-title")))
+    )
+    return loader.load()
 
