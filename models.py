4de57004eed29bdce6e72303ebe548df93d1fd6c
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-02-08 20:30:32 +0100

    Update models.py
    code for loading llm

--- models.py
+++ models.py
@@ -1,1 +1,7 @@
+# models.py - Load Llama Model
+from langchain_community.llms import LlamaCpp
+import os
+
+MODEL_PATH = os.getenv("MODEL_PATH", "models/llama-2-7b.Q5_K_M.gguf")
+llm = LlamaCpp(model_path=MODEL_PATH)
 
