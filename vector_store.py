e9c6c9c728415f095a071e7d3f49b28d18591277
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-02-08 20:33:18 +0100

    Update vector_store.py
    storing data in a vector

--- vector_store.py
+++ vector_store.py
@@ -1,1 +1,8 @@
+# vector_store.py - Embeddings & ChromaDB
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+def create_vector_store(docs):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return Chroma.from_documents(docs, embeddings)
 
