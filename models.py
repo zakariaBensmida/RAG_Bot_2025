128bd31c4379f383a2105bddbf7bb0f332350635
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-02-08 21:31:21 +0100

    Update models.py
    llm parameters change

--- models.py
+++ models.py
@@ -3,5 +3,11 @@ from langchain_community.llms import LlamaCpp
 import os
 
 MODEL_PATH = r"C:\Users\zakar\Chatbot\models\llama-2-7b.Q5_K_M.gguf"
-llm = LlamaCpp(model_path=MODEL_PATH)
+llm = LlamaCpp(
+    model_path=MODEL_PATH,
+    n_ctx=4096,  # Set the context length
+    temperature=0.7,  # Control response randomness
+    max_tokens=512  # Limit output length
+)
+
 
